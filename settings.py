ddf5a29f1da538d00915e978409f80546bb8a416
proplayer2020 <116555319+proplayer2020@users.noreply.github.com>
2023-08-30 23:30:27 +0200

    Add files via upload

--- settings.py
+++ settings.py
@@ -9,12 +9,12 @@ TEST_LOC = "result/test.txt"
 NAMES = "result/obj.names"
 BACKUP_LOC = "result/backup/"
 
-if YOLO_TINY == False:
-    WEIGHTS_LOC = "result/yolov4.weights"
-    CFG_LOC = "result/yolo-obj.cfg"
-else:
-    WEIGHTS_LOC = "result/yolov4-tiny.weights"
-    CFG_LOC = "result/yolo-tiny-obj.cfg"
+
+WEIGHTS_LOC = "result/yolov4.weights"
+CFG_LOC = "result/yolo-obj.cfg"
+
+TINY_WEIGHTS_LOC = "result/yolov4-tiny.weights"
+TINY_CFG_LOC = "result/yolo-tiny-obj.cfg"
 
 ### Do not modify this ###
 
@@ -23,3 +23,6 @@ TEST_LOC = os.path.abspath(TEST_LOC)
 NAMES = os.path.abspath(NAMES)
 BACKUP_LOC = os.path.abspath(BACKUP_LOC)
 WEIGHTS_LOC = os.path.abspath(WEIGHTS_LOC)
+CFG_LOC = os.path.abspath(CFG_LOC)
+TINY_WEIGHTS_LOC = os.path.abspath(TINY_WEIGHTS_LOC)
+TINY_CFG_LOC = os.path.abspath(TINY_CFG_LOC)
